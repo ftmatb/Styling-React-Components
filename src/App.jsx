855d92368ed652539c85377bcf5d3436d2ac9757
Sofi9371 <146138151+ftmatb@users.noreply.github.com>
2025-08-05 00:01:48 +0200

    end of section 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import AuthInputs from "./components/AuthInputs.jsx";
 import Header from "./components/Header.jsx";
 
+
+
+
+
 export default function App() {
+
   return (
     <>
       <Header />
       <main>
+        
         <AuthInputs />
       </main>
     </>
